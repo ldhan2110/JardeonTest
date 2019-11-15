5d944a67078cd803c1af6b5a7634417d43407c9b
LeAn <ldhan21101999@gmail.com>
2019-11-15 12:46:37 +0700

    Initial

--- index.js
+++ index.js
@@ -8,4 +8,5 @@ app.use(express.static(__dirname));
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => {
     console.log(`server is listening on port ${app.get('port')}`);
+    console.log(__dirname)
 });
